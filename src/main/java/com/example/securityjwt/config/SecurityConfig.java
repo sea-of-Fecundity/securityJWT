890dd68aa6e07594028899d2fdd840448106b1de
LetgoMoveNotLate <hj981203@gmail.com>
2024-02-16 21:36:27 +0900

    Feat: cors 설정 front 주소는 localhost:3000으로 임의로 지정함

--- src/main/java/com/example/securityjwt/config/SecurityConfig.java
+++ src/main/java/com/example/securityjwt/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.example.securityjwt.jwt.JwtUtil;
 import com.example.securityjwt.jwt.LoginFilter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,16 +18,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Value("${login.url}")
+    private String login_url;
+
+    @Value("${front.url}")
+    private String front_url;
     private final AuthenticationConfiguration authenticationConfiguration;
     private final ObjectMapper objectMapper;
     private final JwtUtil jwtUtil;
-    private final static String LOGIN_URL = "/login";
 
 
     @Bean
@@ -37,6 +45,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+
+                    configuration.setAllowedOrigins(Collections.singletonList(front_url));
+                    configuration.setAllowedMethods(Collections.singletonList("*"));
+                    configuration.setAllowCredentials(true);
+                    configuration.setAllowedHeaders(Collections.singletonList("*"));
+                    configuration.setMaxAge(3600L);
+
+                    configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                    return configuration;
+                })));
+
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/admin", "/join").permitAll()
@@ -47,7 +71,7 @@ public class SecurityConfig {
         http
                 .addFilterAt(LoginFilter.builder()
                                 .jwtUtil(jwtUtil)
-                                .defaultFilterProcessesUrl(LOGIN_URL)
+                                .defaultFilterProcessesUrl(login_url)
                                 .objectMapper(objectMapper)
                                 .authenticationManager(authenticationManager(authenticationConfiguration))
                                 .build(),
